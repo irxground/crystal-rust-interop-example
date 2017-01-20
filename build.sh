dc67fcaa2e4bb89e37065623ff87925babc870ee
irxground <irxnjhtchlnrw@gmail.com>
2017-01-20 17:58:59 +0900

    update build script

--- build.sh
+++ build.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-set -ex
+set -Ceu
 
 function build_dev() {
+  set -x
   cargo build --manifest-path rust/Cargo.toml
-  mkdir -p bin
-  crystal build src/main.cr -o bin/main-dev \
+  mkdir -p bin-debug
+  crystal build src/main.cr -o bin-debug/main \
     --link-flags ${PWD}/rust/target/debug/libperson.a
 }
 
 function build_release() {
+  set -x
   cargo build --release --manifest-path rust/Cargo.toml
   mkdir -p bin
-  crystal build --release src/main.cr -o bin/main-dev \
+  crystal build --release src/main.cr -o bin/main \
     --link-flags ${PWD}/rust/target/release/libperson.a
 }
 
 function clean() {
+  set -x
   cargo clean --manifest-path rust/Cargo.toml
+  rm -rf bin-debug
   rm -rf bin
 }
 
-case "$1" in
+case "${1-debug}" in
   "release" ) build_release;;
   "clean" ) clean;;
-  * ) build_dev;;
+  "debug" ) build_dev;;
 esac
 
